ac8b6840c21475e49f276cb55e955925ce62b0f5
Dukát Nándor <dukatnandor@gmail.com>
2026-01-04 13:36:17 +0100

    fixed TabList Error (NullPointerException)

--- core/src/main/java/dev/nandi0813/practice/manager/playerdisplay/tab/TabList.java
+++ core/src/main/java/dev/nandi0813/practice/manager/playerdisplay/tab/TabList.java
@@ -3,8 +3,11 @@ package dev.nandi0813.practice.manager.playerdisplay.tab;
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.protocol.player.User;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerPlayerListHeaderAndFooter;
+import dev.nandi0813.practice.ZonePractice;
 import dev.nandi0813.practice.util.PAPIUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
 
 public class TabList {
 
@@ -22,8 +25,17 @@ public class TabList {
                 PAPIUtil.runThroughFormat(player, TabListManager.FOOTER_TEXT)
         );
 
-        User user = PacketEvents.getAPI().getPlayerManager().getUser(player);
-        user.sendPacket(packet);
+        @Nullable User user = PacketEvents.getAPI().getPlayerManager().getUser(player);
+        if (user != null) {
+            user.sendPacket(packet);
+        } else {
+            Bukkit.getScheduler().runTaskLater(ZonePractice.getInstance(), () -> {
+                User delayedUser = PacketEvents.getAPI().getPlayerManager().getUser(player);
+                if (delayedUser != null) {
+                    delayedUser.sendPacket(packet);
+                }
+            }, 1L);
+        }
     }
 
 }
