c66c623e9d13a65bfe9c7f184d46ddf0fcdc609a
yowsef <70838519+aboreda12@users.noreply.github.com>
2026-04-20 07:53:53 +0200

    execute extra items commands as player instead of console (#493)

--- core/src/main/java/dev/nandi0813/practice/manager/inventory/inventoryitem/ExtraInvItem.java
+++ core/src/main/java/dev/nandi0813/practice/manager/inventory/inventoryitem/ExtraInvItem.java
@@ -1,7 +1,6 @@
 package dev.nandi0813.practice.manager.inventory.inventoryitem;
 
 import dev.nandi0813.practice.manager.inventory.InventoryManager;
-import dev.nandi0813.practice.manager.server.ServerManager;
 import org.bukkit.entity.Player;
 
 public class ExtraInvItem extends InvItem {
@@ -26,7 +25,7 @@ public class ExtraInvItem extends InvItem {
             command = command.substring(1);
         }
 
-        ServerManager.runConsoleCommand(command.replace("%player%", player.getName()));
+        player.performCommand(command.replace("%player%", player.getName()));
     }
 
 }
