48c496c089bbe1f51e0dda119f2c63bbc5983e08
lokspel <208148594+lokspel@users.noreply.github.com>
2026-05-26 19:46:21 +0400

    fix: prevent settings being overwritten with defaults on server start

--- core/src/main/java/dev/nandi0813/practice/manager/profile/Profile.java
+++ core/src/main/java/dev/nandi0813/practice/manager/profile/Profile.java
@@ -128,6 +128,10 @@ public class Profile {
     // Data persistence
 
     public void saveData() {
+        if (!fullDataLoaded) {
+            return;
+        }
+
         rankedBan.saveToConfig(file.getConfig(), "ranked-ban");
 
         saveCustomLadders();
