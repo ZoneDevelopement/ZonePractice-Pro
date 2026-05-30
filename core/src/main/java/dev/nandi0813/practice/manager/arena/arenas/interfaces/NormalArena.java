d07bd5ae052d51e6504ce67135bfe8309e6876a2
lokspel <208148594+lokspel@users.noreply.github.com>
2026-05-30 17:05:48 +0400

    Remove useless line

--- core/src/main/java/dev/nandi0813/practice/manager/arena/arenas/interfaces/NormalArena.java
+++ core/src/main/java/dev/nandi0813/practice/manager/arena/arenas/interfaces/NormalArena.java
@@ -15,7 +15,6 @@ import java.util.List;
 @Setter
 public abstract class NormalArena extends BasicArena implements DisplayName {
 
-    // Not used yet
     protected boolean portalProtection = false;
     protected int portalProtectionValue;
 
